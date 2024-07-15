9e11994f2f1b556101dcec0dfc04ed42d015062e
zhaleh <zhalehelmi@gmail.com>
2024-07-15 18:34:12 +0330

    completed signin

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,18 +1,28 @@
-import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
       <div className="h-full lg:flex flex-col items-center justify-center px-4 ">
-        <div className="text-center space-y-4 pt-16 pb-4">
+        <div className="text-center space-y-4 pt-4 ">
           <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
           <p className="text-base text-[#7e8CA0]">
             Log in or Create account to get back to your dashboard!
           </p>
         </div>
-        <div className="flex items-center justify-center mt-8">
-          <SignIn path="/sign-in" />
+        <div className="flex items-center justify-center mt-6">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
         </div>
+      </div>
+      <div className="h-full bg-gray-700 hidden lg:flex items-center justify-center">
+        <Image src="/logo.svg" width={100} height={100} alt="Logo" />
       </div>
     </div>
   );
