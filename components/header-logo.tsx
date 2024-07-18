92f39f6dfd4c27d979fd357644681ffb26ed7f04
zhaleh <zhalehelmi@gmail.com>
2024-07-18 22:03:01 +0330

    add navigation and navbutton

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -6,6 +6,8 @@ const HeaderLogo = () => {
     <Link href="/">
       <div className="items-center hidden lg:flex">
         <Image src="/logo.svg" width={28} height={28} alt="Logo" />
+
+        <p className="font-semibold text-white text-2xl ml-2.5">Finance</p>
       </div>
     </Link>
   );
---
+++ components/navigation.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { usePathname } from "next/navigation";
+import NavButton from "./nav-button";
+
+const routes = [
+  {
+    href: "/",
+    label: "Overview",
+  },
+  {
+    href: "/transactions",
+    label: "Transactions",
+  },
+  {
+    href: "/accounts",
+    label: "Accounts ",
+  },
+  {
+    href: "/categories",
+    label: "Categories",
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+  },
+];
+
+const Navigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav
+      className="hidden lg:flex items-center gap-x-2 
+  overflow-x-auto"
+    >
+      {routes.map((route) => (
+        <NavButton
+          key={route.href}
+          href={route.href}
+          label={route.label}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </nav>
+  );
+};
+
+export default Navigation;
