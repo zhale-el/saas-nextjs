c3bf5b4ec83e358eb8791b5c62d527fdd6f87a3a
zhaleh <zhalehelmi@gmail.com>
2024-07-23 12:43:03 +0330

    schema complete

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,4 +1,5 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 export const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
