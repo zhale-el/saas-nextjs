5c95f02b6ed2d73f2fd0329ac4059aa8ebd47e6c
zhaleh <zhalehelmi@gmail.com>
2024-07-24 15:44:41 +0330

    add post route

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,33 +1,63 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { HTTPException } from "hono/http-exception";
 import { eq } from "drizzle-orm";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
 
-const app = new Hono().get(
-  "/",
-  clerkMiddleware(),
+const app = new Hono()
+  .get(
+    "/",
+    clerkMiddleware(),
 
-  async (c) => {
-    const auth = getAuth(c);
+    async (c) => {
+      const auth = getAuth(c);
 
-    if (!auth?.userId) {
-      throw new HTTPException(401, {
-        res: c.json({ error: "Unauthorized" }, 401),
-      });
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = await db
+        .select({
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(eq(accounts.userId, auth.userId));
+
+      return c.json({ data });
     }
+  )
+  .post(
+    "/",
+    clerkMiddleware(),
 
-    const data = await db
-      .select({
-        id: accounts.id,
-        name: accounts.name,
+    zValidator(
+      "json",
+      insertAccountSchema.pick({
+        name: true,
       })
-      .from(accounts)
-      .where(eq(accounts.userId, auth.userId));
+    ),
 
-    return c.json({ data });
-  }
-);
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+
+      return c.json({ data });
+    }
+  );
 
 export default app;
