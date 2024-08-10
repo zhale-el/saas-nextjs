a9390d4f61ed552c535abb7e3a18af97fc31766b
zhaleh <zhalehelmi@gmail.com>
2024-08-10 18:53:29 +0330

    continue data card

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -1,8 +1,18 @@
 import { IconType } from "react-icons";
-import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "@/lib/utils";
 
-const boxVariant = cva("rounded-md p-3", {
+import { VariantProps, cva } from "class-variance-authority";
+
+import { cn, formatCurrency } from "@/lib/utils";
+import { CountUp } from "./count-up";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+const boxVariant = cva("shrink-0 rounded-md p-3", {
   variants: {
     variant: {
       default: "bg-blue-500/20",
@@ -19,10 +29,10 @@ const boxVariant = cva("rounded-md p-3", {
 const iconVariant = cva("size-6", {
   variants: {
     variant: {
-      default: "fil-blue-500",
-      success: "fil-emerald-500",
-      danger: "fil-rose-500",
-      warning: "fil-yellow-500",
+      default: "fill-blue-500",
+      success: "fill-emerald-500",
+      danger: "fill-rose-500",
+      warning: "fill-yellow-500",
     },
   },
   defaultVariants: {
@@ -30,8 +40,52 @@ const iconVariant = cva("size-6", {
   },
 });
 
-const DataCard = () => {
-  return <div>Enter</div>;
+type BoxVariants = VariantProps<typeof boxVariant>;
+type IconVariants = VariantProps<typeof iconVariant>;
+
+interface DataCardProps extends BoxVariants, IconVariants {
+  icon: IconType;
+  title: string;
+  value?: number;
+  dateRange: string;
+  percentageChange?: number;
+}
+
+const DataCard = ({
+  icon: Icon,
+  title,
+  value = 0,
+  variant,
+  dateRange,
+  percentageChange = 0,
+}: DataCardProps) => {
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+        <div className="space-y-2">
+          <CardTitle className="text-2xl line-clamp-1">{title}</CardTitle>
+          <CardDescription className="line-clamp-1">
+            {dateRange}
+          </CardDescription>
+        </div>
+
+        <div className={cn(boxVariant({ variant }))}>
+          <Icon className={cn(iconVariant({ variant }))} />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <h1 className="font-bold text-2xl mb-2 line-clamp-1 break-all">
+          <CountUp
+            preserveValue
+            start={0}
+            end={value}
+            decimals={2}
+            formattingFn={formatCurrency}
+          />
+        </h1>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default DataCard;
